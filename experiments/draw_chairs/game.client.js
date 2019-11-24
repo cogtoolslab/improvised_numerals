389b21905d879c7aca67a673d0a228ca5e19706e
Sebastian Holt <sholt@ucsd.edu>
2019-11-24 08:49:16 +0000

    add log(commands[3])

--- experiments/draw_chairs/game.client.js
+++ experiments/draw_chairs/game.client.js
@@ -198,6 +198,7 @@ var client_onMessage = function(data) {
       $('#startbutton').hide();
       $('#confirmbutton').hide();
       var num_players = commanddata;
+      console.log(commands[3]); // does this work? sebholt
       client_onjoingame(num_players, commands[3]); break;
 
     case 'add_player' : // New player joined... Need to add them to our list.
