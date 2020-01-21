bdc66cb8126bb805491543e3133b04b300c6c0ff
Sebastian Holt <sholt@ucsd.edu>
2020-01-21 19:02:43 +0000

    it seems two successive console.log statement don't both print

--- experiments/draw_chairs/game.core.js
+++ experiments/draw_chairs/game.core.js
@@ -366,6 +366,7 @@ game_core.prototype.getRandomizedConditions = function() {
 
   // build session by concatenating pre, repeated, and post phases
   var session = _.concat(pre, repeated, post);
+  console.log(pre) // sebholt print statement
   console.log("TEST PRINT WHERE IS IT:") // sebholt print statement
 
   // this is the design dictionary
