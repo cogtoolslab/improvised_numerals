fddfa62864b22833cc7fa2ec580b53ddebc15a23
Judy Fan <judithfan@gmail.com>
2020-04-03 17:54:44 -0700

    change paths to utils and get rid of helpers

--- utils/utils.py
+++ utils/utils.py
@@ -23,6 +23,11 @@ from IPython.display import clear_output
 import importlib
 
 import warnings
+    
+from glob import glob
+import os, sys
+import numpy as np
+import re
 
 def render_images(D, 
                  data = 'pngData',
@@ -62,3 +67,25 @@ def render_images(D,
             clear_output(wait=True) 
     
     print('Done rendering {} images to {}.'.format(D.shape[0],out_dir))
+    
+
+def list_files(path, ext='png'):
+    result = [y for x in os.walk(path) for y in glob(os.path.join(x[0], '*.%s' % ext))]
+    return result
+
+def tryint(s):
+    try:
+        return int(s)
+    except ValueError:
+        return s
+     
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+
+def sort_nicely(l):
+    """ Sort the given list in the way that humans expect.
+    """
+    l.sort(key=alphanum_key)    
