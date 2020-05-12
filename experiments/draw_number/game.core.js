520552a782d943016471906ba9798bf257656f21
Sebastian Holt <sholt@ucsd.edu>
2020-05-12 19:02:41 +0000

    changing iterationName to run4

--- experiments/draw_number/game.core.js
+++ experiments/draw_number/game.core.js
@@ -27,7 +27,7 @@ var game_core = function(options){
   this.server = options.server ;
   this.projectName = 'iterated_number';
   this.experimentName = 'num8_shape4';
-  this.iterationName = 'sandbox3';
+  this.iterationName = 'run4';
   this.email = 'ladlab.ucsd@gmail.com';
   // console.log("color randomized");
 
